18f5ba35dc1814225e03e2c069bf42b8c4feda52
Andrii Vozniuk <a.vozniuk00@gmail.com>
2023-06-09 20:14:08 +0200

    feat(SAV-11): add module integration tests

---
+++ src/Modules/Notifications/Application/UserNotificationSettings/GetUserNotificationSettings/GetUserNotificationSettingsQuery.cs
@@ -0,0 +1,13 @@
+using App.Modules.Notifications.Application.Contracts;
+
+namespace App.Modules.Notifications.Application.UserNotificationSettings.GetUserNotificationSettings;
+
+public class GetUserNotificationSettingsQuery : QueryBase<UserNotificationSettingsDto>
+{
+    public Guid UserId { get; }
+
+    public GetUserNotificationSettingsQuery(Guid userId)
+    {
+        UserId = userId;
+    }
+}
---
+++ src/Tests/IntegrationTests/Usings.cs
@@ -0,0 +1,2 @@
+global using NUnit.Framework;
+global using App.IntegrationTests.SeedWork;
