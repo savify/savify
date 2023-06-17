63470c29981c793696cff17af619c2afb27c60b2
adnylortym <adnylortym@gmail.com>
2023-06-17 21:19:18 +0200

    chore(SAV-21): add warning comment

--- src/Modules/Notifications/Tests/IntegrationTests/Users/SendUserRegistrationConfirmationEmailTests.cs
+++ src/Modules/Notifications/Tests/IntegrationTests/Users/SendUserRegistrationConfirmationEmailTests.cs
@@ -22,6 +22,7 @@ namespace App.Modules.Notifications.IntegrationTests.Users
                 "en",
                 "ABC123"));
 
+            //Other way to check the emails sending should be found here. IEmailSender is not mocked, but injected directly from DI, so the following line does not work as expected.
             await emailSender.Received(Quantity.Exactly(1)).SendEmailAsync(Arg.Is<EmailMessage>(e => e.To.Contains("test@email.com")));
         }
     }
