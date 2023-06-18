57ed8b38c4785e9751f2191f502a2136fb3f5087
Andrii <a.vozniuk00@gmail.com>
2023-06-18 18:02:34 +0200

    chore(SAV-37): add migration

--- src/Modules/Accounts/Infrastructure/AccountsContext.cs
+++ src/Modules/Accounts/Infrastructure/AccountsContext.cs
@@ -1,9 +1,11 @@
 using App.BuildingBlocks.Application.Outbox;
 using App.BuildingBlocks.Infrastructure.Inbox;
 using App.BuildingBlocks.Infrastructure.InternalCommands;
+using App.Modules.Accounts.Domain.Accounts.AccountViewMetadata;
 using App.Modules.Accounts.Domain.Accounts.CashAccounts;
 using App.Modules.Accounts.Domain.Accounts.CreditAccounts;
 using App.Modules.Accounts.Domain.Accounts.DebitAccounts;
+using App.Modules.Accounts.Infrastructure.Domain.Accounts.AccountViewMetadata;
 using App.Modules.Accounts.Infrastructure.Domain.Accounts.CashAccounts;
 using App.Modules.Accounts.Infrastructure.Domain.Accounts.CreditAccounts;
 using App.Modules.Accounts.Infrastructure.Domain.Accounts.DebitAccounts;
@@ -21,6 +23,8 @@ public class AccountsContext : DbContext
     public DbSet<CreditAccount>? CreditAccounts { get; set; }
     
     public DbSet<DebitAccount>? DebitAccounts { get; set; }
+    
+    public DbSet<AccountViewMetadata>? AccountViewMetadata { get; set; }
 
     public DbSet<OutboxMessage>? OutboxMessages { get; set; }
     
@@ -37,6 +41,7 @@ public class AccountsContext : DbContext
         modelBuilder.ApplyConfiguration(new CashAccountEntityTypeConfiguration());
         modelBuilder.ApplyConfiguration(new CreditAccountsEntityTypeConfiguration());
         modelBuilder.ApplyConfiguration(new DebitAccountEntityTypeConfiguration());
+        modelBuilder.ApplyConfiguration(new AccountViewMetadataEntityTypeConfiguration());
         modelBuilder.ApplyConfiguration(new OutboxMessageEntityTypeConfiguration());
         modelBuilder.ApplyConfiguration(new InboxMessageEntityTypeConfiguration());
         modelBuilder.ApplyConfiguration(new InternalCommandEntityTypeConfiguration());
