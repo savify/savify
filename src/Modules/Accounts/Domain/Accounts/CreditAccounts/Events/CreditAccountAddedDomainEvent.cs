e3575b0dbbd27399386e8919ae1e10534dcc0de9
adnylortym <adnylortym@gmail.com>
2023-06-18 15:37:09 +0200

    feat(SAV-33): add credit account domain model

---
+++ src/Modules/Accounts/Domain/Accounts/CreditAccounts/Events/CreditAccountAddedDomainEvent.cs
@@ -0,0 +1,20 @@
+﻿using App.BuildingBlocks.Domain;
+using App.Modules.Accounts.Domain.Users;
+
+namespace App.Modules.Accounts.Domain.Accounts.CreditAccounts.Events;
+
+public class CreditAccountAddedDomainEvent : DomainEventBase
+{
+    public AccountId AccountId { get; }
+
+    public UserId UserId { get; }
+
+    public Currency Currency { get; }
+
+    public CreditAccountAddedDomainEvent(AccountId accountId, UserId userId, Currency currency)
+    {
+        AccountId = accountId;
+        UserId = userId;
+        Currency = currency;
+    }
+}
