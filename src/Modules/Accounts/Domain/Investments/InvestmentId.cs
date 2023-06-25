85d699a96154426b3bb23837c8ba2b17210b0dad
adnylortym <adnylortym@gmail.com>
2023-06-25 08:27:50 +0200

    feat(SAV-39): add investment id

---
+++ src/Modules/Accounts/Domain/Investments/InvestmentId.cs
@@ -0,0 +1,9 @@
+﻿using App.BuildingBlocks.Domain;
+
+namespace App.Modules.Accounts.Domain.Investments;
+public class InvestmentId : TypedIdValueBase
+{
+    public InvestmentId(Guid value) : base(value)
+    {
+    }
+}
