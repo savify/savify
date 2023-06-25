f0e4071583709f086792d2511af9b64a187dc7db
adnylortym <adnylortym@gmail.com>
2023-06-25 15:20:02 +0200

    chore(SAV-35): add get cash account query

---
+++ src/Modules/Accounts/Application/CashAccounts/GetCashAccount/CashAccountDto.cs
@@ -0,0 +1,10 @@
+﻿namespace App.Modules.Accounts.Application.CashAccounts.GetCashAccount;
+public class CashAccountDto
+{
+    public Guid Id { get; set; }
+    public Guid UserId { get; set; }
+    public string Title { get; set; }
+    public string Currency { get; set; }
+    public int Balance { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
---
+++ src/Modules/Accounts/Application/CashAccounts/GetCashAccount/GetCashAccountQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using App.BuildingBlocks.Application.Data;
+using App.Modules.Accounts.Application.Configuration.Queries;
+using Dapper;
+
+namespace App.Modules.Accounts.Application.CashAccounts.GetCashAccount;
+internal class GetCashAccountQueryHandler : IQueryHandler<GetCashAccountQuery, CashAccountDto>
+{
+    private readonly ISqlConnectionFactory _sqlConnectionFactory;
+
+    public GetCashAccountQueryHandler(ISqlConnectionFactory sqlConnectionFactory)
+    {
+        _sqlConnectionFactory = sqlConnectionFactory;
+    }
+
+    public async Task<CashAccountDto> Handle(GetCashAccountQuery request, CancellationToken cancellationToken)
+    {
+        using var connection = _sqlConnectionFactory.GetOpenConnection();
+
+        string sql = "SELECT id, user_id As userId, title, currency, balance, created_at AS createdAt" +
+                     "FROM accounts.cash_accounts " +
+                     "WHERE id = @AccountId";
+
+        return await connection.QuerySingleOrDefaultAsync<CashAccountDto>(sql, new { request.AccountId });
+    }
+}
