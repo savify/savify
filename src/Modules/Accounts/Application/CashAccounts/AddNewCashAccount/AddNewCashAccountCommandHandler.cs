edd33db393687bdcad2e87cd83e98f675a903272
adnylortym <adnylortym@gmail.com>
2023-06-25 14:37:08 +0200

    feat(SAV-35): add new cash account command

---
+++ src/Modules/Accounts/Application/CashAccounts/AddNewCashAccount/AddNewCashAccountCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using App.Modules.Accounts.Application.Configuration.Commands;
+using App.Modules.Accounts.Domain.Accounts;
+using App.Modules.Accounts.Domain.Accounts.CashAccounts;
+using App.Modules.Accounts.Domain.Users;
+
+namespace App.Modules.Accounts.Application.CashAccounts.AddNewCashAccount;
+
+internal class AddNewCashAccountCommandHandler : ICommandHandler<AddNewCashAccountCommand, Guid>
+{
+    private readonly ICashAccountRepository _repository;
+
+    public AddNewCashAccountCommandHandler(ICashAccountRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<Guid> Handle(AddNewCashAccountCommand request, CancellationToken cancellationToken)
+    {
+        var cashAccount = CashAccount.AddNew(
+            new UserId(request.UserId),
+            request.Title,
+            Currency.From(request.Currency),
+            request.Balance);
+
+        await _repository.AddAsync(cashAccount);
+
+        return cashAccount.Id.Value;
+    }
+}
---
+++ src/Modules/Accounts/Application/CashAccounts/AddNewCashAccount/AddNewCashAccountCommandValidator.cs
@@ -0,0 +1,23 @@
+﻿using App.BuildingBlocks.Application.Validators;
+using FluentValidation;
+
+namespace App.Modules.Accounts.Application.CashAccounts.AddNewCashAccount;
+internal class AddNewCashAccountCommandValidator : Validator<AddNewCashAccountCommand>
+{
+    public AddNewCashAccountCommandValidator()
+    {
+        RuleFor(c => c.Title)
+            .NotEmpty()
+            .WithMessage("Please probide the new cash account title");
+
+        RuleFor(c => c.Currency)
+            .NotEmpty()
+            .WithMessage("Please provide the new cash account currenty")
+            .Length(3)
+            .WithMessage("Currency should be provided in currency code format (ISO 4217).");
+
+        RuleFor(c => c.Balance)
+            .GreaterThanOrEqualTo(0)
+            .WithMessage("Balance should be greater or equal to zero");
+    }
+}
