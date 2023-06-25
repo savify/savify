c49a52fdf1eafdddbfcefb25114a880880675ba3
adnylortym <adnylortym@gmail.com>
2023-06-25 08:57:22 +0200

    chore(SAV-39): add model

---
+++ src/Modules/Accounts/Domain/Portfolios/InvestmentPortfolios/Events/InvestmentPortfolioCreatedDomainEvent.cs
@@ -0,0 +1,21 @@
+﻿using App.BuildingBlocks.Domain;
+using App.Modules.Accounts.Domain.Accounts;
+using App.Modules.Accounts.Domain.Investments;
+using App.Modules.Accounts.Domain.Users;
+
+namespace App.Modules.Accounts.Domain.Portfolios.InvestmentPortfolios.Events;
+public class InvestmentPortfolioCreatedDomainEvent : DomainEventBase
+{
+    public PortfolioId PortfolioId { get; }
+
+    public UserId UserId { get; }
+
+    public Currency Currency { get; }
+
+    public InvestmentPortfolioCreatedDomainEvent(PortfolioId portfolioId, UserId userId, Currency currency)
+    {
+        PortfolioId = portfolioId;
+        UserId = userId;
+        Currency = currency;
+    }
+}
--- src/Modules/Accounts/Domain/Portfolios/InvestmentPortfolios/InvestmentPortfolio.cs
+++ src/Modules/Accounts/Domain/Portfolios/InvestmentPortfolios/InvestmentPortfolio.cs
@@ -1,11 +1,36 @@
 ﻿using App.BuildingBlocks.Domain;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using App.Modules.Accounts.Domain.Accounts;
+using App.Modules.Accounts.Domain.Portfolios.InvestmentPortfolios.Events;
+using App.Modules.Accounts.Domain.Users;
 
 namespace App.Modules.Accounts.Domain.Investments.InvestmentPortfolios;
 public class InvestmentPortfolio : Entity, IAggregateRoot
 {
+    public PortfolioId Id { get; }
+
+    internal UserId UserId { get; }
+
+    private string _title;
+
+    //Do we still want to have currency on investment portfolio?
+    //Maybe we could store something more abstract than money - like investment type and amount.
+    private Currency _currency;
+
+    private DateTime _createdAt;
+
+    public static InvestmentPortfolio AddNew(UserId userId, string title, Currency currency)
+    {
+        return new InvestmentPortfolio(userId, title, currency);
+    }
+
+    private InvestmentPortfolio(UserId userId, string title, Currency currency)
+    {
+        Id = new PortfolioId(Guid.NewGuid());
+        UserId = userId;
+        _title = title;
+        _currency = currency;
+        _createdAt = DateTime.UtcNow;
+
+        AddDomainEvent(new InvestmentPortfolioCreatedDomainEvent(Id, UserId, _currency));
+    }
 }
