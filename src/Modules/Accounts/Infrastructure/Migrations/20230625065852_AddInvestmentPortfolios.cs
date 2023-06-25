475c212a6c12b2197b13cb8eb46ccb35e486bf31
adnylortym <adnylortym@gmail.com>
2023-06-25 08:59:22 +0200

    chore(SAV-39): add migration

---
+++ src/Modules/Accounts/Infrastructure/Migrations/20230625065852_AddInvestmentPortfolios.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace App.Modules.Accounts.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddInvestmentPortfolios : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
