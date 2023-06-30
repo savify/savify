c6d0b58d40f8e13a25afb7a11dfd0de6a6e81897
adnylortym <adnylortym@gmail.com>
2023-06-30 19:55:45 +0200

    chore(SAV-35): add test for created view metadata. Test names refactor

---
+++ src/Modules/Accounts/Tests/IntegrationTests/CashAccounts/AddNewCashAccountTests.cs
@@ -0,0 +1,44 @@
+﻿using App.Modules.Accounts.Application.CashAccounts.AddNewCashAccount;
+using App.Modules.Accounts.Application.CashAccounts.GetCashAccount;
+using App.Modules.Accounts.Application.ViewMetadata.GetViewMetadata;
+using App.Modules.Accounts.IntegrationTests.SeedWork;
+
+namespace App.Modules.Accounts.IntegrationTests.CashAccounts;
+
+[TestFixture]
+public class AddNewCashAccountTests : TestBase
+{
+    [Test]
+    public async Task AddNewCashAccountCommand_AddsNewCashAccount()
+    {
+        var command = new AddNewCashAccountCommand(
+            Guid.NewGuid(),
+            "Cash account",
+            "PLN",
+            1000);
+        var accountId = await AccountsModule.ExecuteCommandAsync(command);
+
+        var createdCashAccount = await AccountsModule.ExecuteQueryAsync(new GetCashAccountQuery(accountId));
+
+        Assert.IsNotNull(createdCashAccount);
+        Assert.That(createdCashAccount.UserId, Is.EqualTo(command.UserId));
+        Assert.That(createdCashAccount.Title, Is.EqualTo(command.Title));
+        Assert.That(createdCashAccount.Balance, Is.EqualTo(command.Balance));
+    }
+
+    [Test]
+    public async Task AddNewCashAccountCommand_AddsViewMetadata()
+    {
+        var command = new AddNewCashAccountCommand(
+            Guid.NewGuid(),
+            "Cash account",
+            "PLN",
+            1000);
+        var accountId = await AccountsModule.ExecuteCommandAsync(command);
+
+        var createdViewMetadata = await AccountsModule.ExecuteQueryAsync(new GetAccountViewMetadataQuery(accountId));
+
+        Assert.IsNotNull(createdViewMetadata);
+        Assert.That(createdViewMetadata.AccountId, Is.EqualTo(accountId));
+    }
+}
