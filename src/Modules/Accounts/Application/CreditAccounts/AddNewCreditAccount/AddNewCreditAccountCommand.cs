0f78cfa19eb60106e7b120ac35f2a6da7db8844b
adnylortym <adnylortym@gmail.com>
2023-07-02 10:40:08 +0200

    feat(SAV-59): add new credit card command

---
+++ src/Modules/Accounts/Application/CreditAccounts/AddNewCreditAccount/AddNewCreditAccountCommand.cs
@@ -0,0 +1,20 @@
+﻿using App.Modules.Accounts.Application.Contracts;
+
+namespace App.Modules.Accounts.Application.CreditAccounts.AddNewCreditAccount;
+public class AddNewCreditAccountCommand : CommandBase<Guid>
+{
+    public Guid UserId { get; }
+    public string Title { get; }
+    public int AvailableBalance { get; }
+    public int CreditLimit { get; }
+    public string Currency { get; }
+
+    public AddNewCreditAccountCommand(Guid userId, string title, int availableBalance, int creditLimit, string currency)
+    {
+        UserId = userId;
+        Title = title;
+        AvailableBalance = availableBalance;
+        CreditLimit = creditLimit;
+        Currency = currency;
+    }
+}
---
+++ src/Modules/Accounts/Application/CreditAccounts/AddNewCreditAccount/AddNewCreditAccountCommandValidator.cs
@@ -0,0 +1,30 @@
+﻿using App.BuildingBlocks.Application.Validators;
+using FluentValidation;
+
+namespace App.Modules.Accounts.Application.CreditAccounts.AddNewCreditAccount;
+
+internal class AddNewCreditAccountCommandValidator : Validator<AddNewCreditAccountCommand>
+{
+    public AddNewCreditAccountCommandValidator()
+    {
+        RuleFor(c => c.Title)
+            .NotEmpty()
+            .WithMessage("Please provide the new credit account title");
+
+        RuleFor(c => c.AvailableBalance)
+            .GreaterThanOrEqualTo(0)
+            .WithMessage("AvailableBalance should be greater or equal to zero");
+
+        RuleFor(c => c.CreditLimit)
+            .GreaterThanOrEqualTo(0)
+            .WithMessage("CreditLimit should be greated or equal to zero")
+            .GreaterThanOrEqualTo(cc => cc.AvailableBalance)
+            .WithMessage("Credit limit should be greater or equal to AvailableBalance");
+
+        RuleFor(c => c.Currency)
+            .NotEmpty()
+            .WithMessage("Please provide the new cash account currenty")
+            .Length(3)
+            .WithMessage("Currency should be provided in currency code format (ISO 4217).");
+    }
+}
