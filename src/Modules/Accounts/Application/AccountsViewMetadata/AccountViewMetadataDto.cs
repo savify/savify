6643d4c1d5f2ed82c45bc79162970bb6f7e031db
adnylortym <adnylortym@gmail.com>
2023-07-02 11:12:53 +0200

    chore(SAV-59): get credit account query

---
+++ src/Modules/Accounts/Application/AccountsViewMetadata/AccountViewMetadataDto.cs
@@ -0,0 +1,8 @@
+﻿namespace App.Modules.Accounts.Application.AccountsViewMetadata;
+public class AccountViewMetadataDto
+{
+    public Guid AccountId { get; set; }
+    public string? Color { get; set; }
+    public string? Icon { get; set; }
+    public bool IsConsideredInTotalBalance { get; set; }
+}
---
+++ src/Modules/Accounts/Application/CreditAccounts/GetCreditAccount/GetCreditAccountQuery.cs
@@ -0,0 +1,12 @@
+﻿using App.Modules.Accounts.Application.Contracts;
+
+namespace App.Modules.Accounts.Application.CreditAccounts.GetCreditAccount;
+public class GetCreditAccountQuery : QueryBase<CreditAccountDto>
+{
+    public Guid AccountId { get; }
+
+    public GetCreditAccountQuery(Guid accountId)
+    {
+        AccountId = accountId;
+    }
+}
