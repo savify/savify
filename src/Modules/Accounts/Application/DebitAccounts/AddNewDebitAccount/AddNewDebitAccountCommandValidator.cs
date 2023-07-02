4c5d8691aa5f73bffe20f504024a72452bc31d00
adnylortym <adnylortym@gmail.com>
2023-07-02 13:35:10 +0200

    feat(SAV-58): add command

---
+++ src/Modules/Accounts/Application/DebitAccounts/AddNewDebitAccount/AddNewDebitAccountCommandValidator.cs
@@ -0,0 +1,24 @@
+﻿using App.BuildingBlocks.Application.Validators;
+using FluentValidation;
+
+namespace App.Modules.Accounts.Application.DebitAccounts.AddNewDebitAccount;
+
+internal class AddNewDebitAccountCommandValidator : Validator<AddNewDebitAccountCommand>
+{
+    public AddNewDebitAccountCommandValidator()
+    {
+        RuleFor(c => c.Title)
+            .NotEmpty()
+            .WithMessage("Please provide the new debit account title");
+
+        RuleFor(c => c.Currency)
+            .NotEmpty()
+            .WithMessage("Please provide the new cash account currenty")
+            .Length(3)
+            .WithMessage("Currency should be provided in currency code format (ISO 4217).");
+
+        RuleFor(c => c.Balance)
+            .GreaterThanOrEqualTo(0)
+            .WithMessage("Balance should be greater or equal to zero");
+    }
+}
