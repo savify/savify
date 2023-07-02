53d5f581ecf6bb9c1ab72e1cb794285aefc649d4
adnylortym <adnylortym@gmail.com>
2023-07-02 14:03:08 +0200

    chore(SAV-58): add controller action

---
+++ src/API/Modules/Accounts/DebitAccounts/AddNewDebitAccountRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace App.API.Modules.Accounts.DebitAccounts;
+
+public class AddNewDebitAccountRequest
+{
+    public string Title { get; set; }
+    public string Currency { get; set; }
+    public int Balance { get; set; }
+}
