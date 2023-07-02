388f193f1cc7b9f7896d57d7e99115e749ab5b28
Dmytro Lynda <58661187+DmytroLynda@users.noreply.github.com>
2023-07-02 20:01:59 +0200

    feat(SAV-35): add cash account (#35)
    * feat(SAV-35): add new cash account command
    
    * chore(SAV-35): add get cash account query
    
    * chore(SAV-35): uncomment accounts command decorators
    
    * chore(SAV-35): add cash account integration test
    
    * chore(SAV-35): get account view metadata query
    
    * chore(SAV-35): add test for created view metadata. Test names refactor
    
    * chore(SAV-35): add account view medatada when add new cash account
    
    * fix(SAV-35): account view metadata sql query
    
    * chore(SAV-35): add metadata to cash account model
    
    * fix(SAV-35): balance validation + translations

---
+++ src/API/Modules/Accounts/CashAccounts/AddNewCashAccountRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace App.API.Modules.Accounts.CashAccounts;
+
+public class AddNewCashAccountRequest
+{
+    public string Title { get; set; }
+
+    public string Currency { get; set; }
+
+    public int Balance { get; set; }
+}
---
+++ src/Modules/Accounts/Application/CashAccounts/GetCashAccount/GetCashAccountQuery.cs
@@ -0,0 +1,12 @@
+﻿using App.Modules.Accounts.Application.Contracts;
+
+namespace App.Modules.Accounts.Application.CashAccounts.GetCashAccount;
+public class GetCashAccountQuery : QueryBase<CashAccountDto>
+{
+    public Guid AccountId { get; }
+
+    public GetCashAccountQuery(Guid accountId)
+    {
+        AccountId = accountId;
+    }
+}
