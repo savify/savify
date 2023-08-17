82dad90513007fe8aaf8757a6838b5dcf622aaed
Andrii <a.vozniuk00@gmail.com>
2023-08-17 11:01:41 +0200

    chore(SAV-38): add unit tests

--- src/Modules/Wallets/Tests/UnitTests/Wallets/CashWalletsTests.cs
+++ src/Modules/Wallets/Tests/UnitTests/Wallets/CashWalletsTests.cs
@@ -2,6 +2,7 @@ using App.Modules.Wallets.Domain.Users;
 using App.Modules.Wallets.Domain.Wallets;
 using App.Modules.Wallets.Domain.Wallets.CashWallets;
 using App.Modules.Wallets.Domain.Wallets.CashWallets.Events;
+using App.Modules.Wallets.Domain.Wallets.CashWallets.Rules;
 
 namespace App.Modules.Wallets.UnitTests.Wallets;
 
@@ -34,5 +35,46 @@ public class CashWalletsTests : UnitTestBase
         Assert.That(walletEditedDomainEvent.UserId, Is.EqualTo(userId));
         Assert.That(walletEditedDomainEvent.NewCurrency, Is.EqualTo(new Currency("GBP")));
         Assert.That(walletEditedDomainEvent.NewBalance, Is.EqualTo(2000));
+    }
+
+    [Test]
+    public void EditingCashWallet_WhenWalletIsRemoved_BreaksCashWalletCannotBeEditedIfWasRemovedRule()
+    {
+        var userId = new UserId(Guid.NewGuid());
+        var wallet = CashWallet.AddNew(userId, "Cash", Currency.From("PLN"), 1000);
+
+        wallet.Remove();
+
+        AssertBrokenRule<CashWalletCannotBeEditedIfWasRemovedRule>(() =>
+        {
+            wallet.Edit("New cash", new Currency("GBP"), 2000);
+        });
+    }
+
+    [Test]
+    public void RemovingCashWallet_IsSuccessful()
+    {
+        var userId = new UserId(Guid.NewGuid());
+        var wallet = CashWallet.AddNew(userId, "Cash", Currency.From("PLN"), 1000);
+
+        wallet.Remove();
+
+        var walletRemovedDomainEvent = AssertPublishedDomainEvent<CashWalletRemovedDomainEvent>(wallet);
+        Assert.That(walletRemovedDomainEvent.WalletId, Is.EqualTo(wallet.Id));
+        Assert.That(walletRemovedDomainEvent.UserId, Is.EqualTo(wallet.UserId));
+    }
+
+    [Test]
+    public void RemovingCashWallet_WhenWasAlreadyRemoved_BreaksCashWalletCannotBeRemovedMoreThanOnceRule()
+    {
+        var userId = new UserId(Guid.NewGuid());
+        var wallet = CashWallet.AddNew(userId, "Cash", Currency.From("PLN"), 1000);
+
+        wallet.Remove();
+
+        AssertBrokenRule<CashWalletCannotBeRemovedMoreThanOnceRule>(() =>
+        {
+            wallet.Remove();
+        });
     }
 }
