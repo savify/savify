a8d662d22caf2864f598cbfaf37d23649ea7b293
Andrii <a.vozniuk00@gmail.com>
2023-12-12 17:49:57 +0100

    chore(SAV-125): finance tracking integration tests refactor

--- src/Modules/FinanceTracking/Tests/IntegrationTests/SeedWork/CustomWebApplicationFactory.cs
+++ src/Modules/FinanceTracking/Tests/IntegrationTests/SeedWork/CustomWebApplicationFactory.cs
@@ -1,20 +1,57 @@
 using App.BuildingBlocks.Application;
-using App.BuildingBlocks.Tests.IntegrationTests;
+using App.BuildingBlocks.Application.Data;
+using App.BuildingBlocks.Infrastructure.Data;
+using App.Modules.FinanceTracking.Infrastructure;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
+using Testcontainers.PostgreSql;
 
 namespace App.Modules.FinanceTracking.IntegrationTests.SeedWork;
 
 public class CustomWebApplicationFactory<TProgram> : WebApplicationFactory<TProgram> where TProgram : class
 {
+    private readonly PostgreSqlContainer _dbContainer = new PostgreSqlBuilder()
+        .WithImage("postgres:15-alpine")
+        .WithDatabase("savify")
+        .WithUsername("user")
+        .WithPassword("pass")
+        .Build();
+
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
         builder.ConfigureTestServices(services =>
         {
+            var dbContextDescriptor = services.SingleOrDefault(s => s.ServiceType == typeof(DbContextOptions<FinanceTrackingContext>));
+
+            if (dbContextDescriptor is not null)
+            {
+                services.Remove(dbContextDescriptor);
+            }
+
+            services.AddDbContext<FinanceTrackingContext>(options =>
+            {
+                options.UseNpgsql(_dbContainer.GetConnectionString());
+                options.ReplaceService<IValueConverterSelector, StronglyTypedIdValueConverterSelector>();
+            });
+            services.Replace(ServiceDescriptor.Scoped<ISqlConnectionFactory>(_ => new SqlConnectionFactory(_dbContainer.GetConnectionString())));
             services.Replace(ServiceDescriptor.Scoped<IExecutionContextAccessor>(_ => new ExecutionContextMock(Guid.NewGuid())));
         });
     }
+
+    public Task InitialiseDbContainerAsync()
+    {
+        return _dbContainer.StartAsync();
+    }
+
+    public Task DisposeDbContainerAsync()
+    {
+        return _dbContainer.StopAsync();
+    }
+
+    public string GetConnectionString() => _dbContainer.GetConnectionString();
 }
