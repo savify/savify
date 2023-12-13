76f25eddd5fe679ced45e4a2fe84c22bdc6e99c5
Andrii <a.vozniuk00@gmail.com>
2023-12-13 17:50:00 +0100

    chore(SAV-127): migrate Banks

--- src/Modules/Banks/Infrastructure/Migrations/20231213164802_EFMigration.Designer.cs
+++ src/Modules/Banks/Infrastructure/Migrations/20231213164802_EFMigration.Designer.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using App.Modules.Banks.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -12,9 +13,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace App.Modules.Banks.Infrastructure.Migrations
 {
     [DbContext(typeof(BanksContext))]
-    partial class BanksContextModelSnapshot : ModelSnapshot
+    [Migration("20231213164802_EFMigration")]
+    partial class EFMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
