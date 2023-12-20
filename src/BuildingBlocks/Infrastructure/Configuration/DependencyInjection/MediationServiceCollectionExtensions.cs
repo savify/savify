f715906575e604bf8681c84c6a800cf2396a247c
Andrii <a.vozniuk00@gmail.com>
2023-12-20 18:25:06 +0100

    chore(SAV-110): fix command validation

--- src/BuildingBlocks/Infrastructure/Configuration/DependencyInjection/MediationServiceCollectionExtensions.cs
+++ src/BuildingBlocks/Infrastructure/Configuration/DependencyInjection/MediationServiceCollectionExtensions.cs
@@ -1,4 +1,5 @@
 using System.Reflection;
+using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace App.BuildingBlocks.Infrastructure.Configuration.DependencyInjection;
@@ -8,6 +9,20 @@ public static class MediationServiceCollectionExtensions
     public static IServiceCollection AddMediatRForAssemblies(this IServiceCollection services, params Assembly[] assemblies)
     {
         services.AddMediatR(configuration => configuration.RegisterServicesFromAssemblies(assemblies));
+
+        var mediatorOpenTypes = new[]
+        {
+            typeof(IValidator<>)
+        };
+
+        foreach (var mediatorOpenType in mediatorOpenTypes)
+        {
+            services.Scan(scan => scan
+                .FromAssemblies(assemblies)
+                .AddClasses(classes => classes.AssignableTo(mediatorOpenType))
+                .AsImplementedInterfaces()
+                .WithScopedLifetime());
+        }
 
         return services;
     }
--- src/Modules/Transactions/Infrastructure/Configuration/DependencyInjection/ProcessingServiceCollectionExtensions.cs
+++ src/Modules/Transactions/Infrastructure/Configuration/DependencyInjection/ProcessingServiceCollectionExtensions.cs
@@ -3,7 +3,6 @@ using App.BuildingBlocks.Infrastructure.DomainEventsDispatching;
 using App.Modules.Transactions.Application.Configuration.Commands;
 using App.Modules.Transactions.Infrastructure.Configuration.Processing.Decorators;
 using App.Modules.Transactions.Infrastructure.Configuration.Processing.InternalCommands;
-using FluentValidation;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -18,8 +17,6 @@ internal static class ProcessingServiceCollectionExtensions
         services.AddScoped<IUnitOfWork<TransactionsContext>, UnitOfWork<TransactionsContext>>();
 
         services.AddScoped<ICommandScheduler, CommandScheduler>();
-
-        services.AddScoped<IList<IValidator>>(provider => provider.GetServices<IValidator>().ToList());
 
         services.Decorate(typeof(IRequestHandler<>), typeof(UnitOfWorkCommandHandlerDecorator<>));
         services.Decorate(typeof(IRequestHandler<>), typeof(ValidationCommandHandlerDecorator<>));
