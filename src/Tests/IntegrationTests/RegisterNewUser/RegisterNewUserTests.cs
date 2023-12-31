a6ec0df74d2502ac368379c665737090e3d00cbf
Andrii <40670081+X0GT0X@users.noreply.github.com>
2023-12-31 14:05:15 +0100

    feat(TESTS): write integration tests for user registation emails sending (#99)

---
+++ src/Tests/IntegrationTests/RegisterNewUser/RegisterNewUserTests.cs
@@ -0,0 +1,64 @@
+using App.BuildingBlocks.Tests.IntegrationTests.Probing;
+using App.Modules.Notifications.Application.Emails;
+using App.Modules.UserAccess.Application.UserRegistrations.RegisterNewUser;
+
+namespace App.IntegrationTests.RegisterNewUser;
+
+[TestFixture]
+public class RegisterNewUserTests : TestBase
+{
+    [Test]
+    public async Task SendUserRegistrationConfirmationEmail_WhenUserRegistrationWasCreated_Test()
+    {
+        await UserAccessModule.ExecuteCommandAsync(new RegisterNewUserCommand(
+            "test@email.com",
+            "Test1234!",
+            "Name",
+            "PL",
+            "en"
+        ));
+
+        await AssertEventually(
+            new GetUserRegistrationConfirmationEmailProbe(
+                "test@email.com",
+                "Savify - Welcome - Please confirm your registration",
+                EmailSender), 20000);
+    }
+
+    private class GetUserRegistrationConfirmationEmailProbe(
+        string expectedRecipientEmailAddress,
+        string expectedEmailSubject,
+        IEmailSender emailSender)
+        : IProbe
+    {
+        private EmailMessage? _emailMessage;
+
+        public bool IsSatisfied()
+        {
+            if (_emailMessage != null)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public Task SampleAsync()
+        {
+            try
+            {
+                _emailMessage = ((EmailSenderMock)emailSender).SentEmails.SingleOrDefault(e =>
+                    e.To.Any(address => address == expectedRecipientEmailAddress) &&
+                    e.Subject == expectedEmailSubject);
+            }
+            catch
+            {
+                // ignored
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public string DescribeFailureTo() => $"User registration confirmation email was not sent to '{expectedRecipientEmailAddress}'";
+    }
+}
