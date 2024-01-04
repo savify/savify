d59534fe555f6fe9839a35c41a37cac7aa29f9f7
Andrii <a.vozniuk00@gmail.com>
2024-01-04 20:47:26 +0100

    chore(SAV-121): expense migration and repo

--- src/Modules/FinanceTracking/Infrastructure/Migrations/20240104194624_AddExpenses.Designer.cs
+++ src/Modules/FinanceTracking/Infrastructure/Migrations/20240104194624_AddExpenses.Designer.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using App.Modules.FinanceTracking.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -12,9 +13,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace App.Modules.FinanceTracking.Infrastructure.Migrations
 {
     [DbContext(typeof(FinanceTrackingContext))]
-    partial class FinanceTrackingContextModelSnapshot : ModelSnapshot
+    [Migration("20240104194624_AddExpenses")]
+    partial class AddExpenses
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Modules/FinanceTracking/Infrastructure/Migrations/20240104194624_AddExpenses.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace App.Modules.FinanceTracking.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddExpenses : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "expenses",
+                schema: "finance_tracking",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    user_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    category_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    comment = table.Column<string>(type: "text", nullable: false),
+                    made_on = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    source_wallet_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    tags = table.Column<string[]>(type: "text[]", nullable: false),
+                    amount = table.Column<int>(type: "integer", nullable: false),
+                    currency = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_expenses", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "expenses",
+                schema: "finance_tracking");
+        }
+    }
+}
