79c4e293ae93676b67f956e0f5ce01bef41c30a6
Andrii <a.vozniuk00@gmail.com>
2024-01-09 10:08:57 +0100

    feat(SAV-169): credit wallets cleanup

--- src/Modules/FinanceTracking/Application/Wallets/CreditWallets/AddNewCreditWallet/AddNewCreditWalletCommandValidator.cs
+++ src/Modules/FinanceTracking/Application/Wallets/CreditWallets/AddNewCreditWallet/AddNewCreditWalletCommandValidator.cs
@@ -18,9 +18,7 @@ internal class AddNewCreditWalletCommandValidator : Validator<AddNewCreditWallet
 
         RuleFor(c => c.AvailableBalance)
             .GreaterThanOrEqualTo(0)
-            .WithMessage("Available balance should be greater or equal to zero")
-            .LessThanOrEqualTo(cc => cc.CreditLimit)
-            .WithMessage("Available balance should be less than or equal to credit limit");
+            .WithMessage("Available balance should be greater or equal to zero");
 
         RuleFor(c => c.Currency)
             .NotEmpty()
